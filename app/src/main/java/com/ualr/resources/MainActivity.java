1a770469a9b18bc05a99ee7002e5e87fc2f47416
irconde <ivanrodriguezconde@gmail.com>
2019-09-03 22:40:22 -0500

    First code commit

---
+++ app/src/main/java/com/ualr/resources/MainActivity.java
@@ -0,0 +1,31 @@
+package com.ualr.resources;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.snackbar.Snackbar;
+
+// TODO 4: Define the alternative resources needed to have different content and look n feel depending on the device language.
+
+public class MainActivity extends AppCompatActivity {
+
+    private CoordinatorLayout rootLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rootLayout = findViewById(R.id.root_layout);
+    }
+
+    public void onLessonLearntClicked(View view) {
+        String congratsMessage = null;
+        // TODO 5: Get congrats_text resource -->
+        // TODO 6: Once congratsMessage is initialized, uncomment the two following lines
+        // Snackbar snackbar = Snackbar.make(rootLayout, congratsMessage, Snackbar.LENGTH_LONG);
+        // snackbar.show();
+    }
+}
