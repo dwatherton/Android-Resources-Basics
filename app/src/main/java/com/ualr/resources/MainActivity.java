7cba837e70d60e7a4b3a62f1992c4db125d32618
Dwatherton <Dwatherton@ualr.edu>
2019-09-15 12:16:55 -0500

    TODO 6: Uncommented the two Snackbar lines now that congratsMessage is initialized.

--- app/src/main/java/com/ualr/resources/MainActivity.java
+++ app/src/main/java/com/ualr/resources/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         // TODO 5: Get congrats_text resource -->
         String congratsMessage = getResources().getString(R.string.congrats_text);
         // TODO 6: Once congratsMessage is initialized, uncomment the two following lines
-        // Snackbar snackbar = Snackbar.make(rootLayout, congratsMessage, Snackbar.LENGTH_LONG);
-        // snackbar.show();
+        Snackbar snackbar = Snackbar.make(rootLayout, congratsMessage, Snackbar.LENGTH_LONG);
+        snackbar.show();
     }
 }
